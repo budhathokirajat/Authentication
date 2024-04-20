fefdf98d6535a861eebcf6c54e91bcb2c583b0f1
Rajat Budhathoki <97826778+budhathokirajat@users.noreply.github.com>
2024-04-20 16:14:17 +0545

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+// src/App.js
+import React from "react";
+import Login from "./Components/Login"; // Adjust the path based on your project structure
+import "./App.css"
+const App = () => {
+  return (
+    <>
+      <Login />
+    </>
+  );
+};
+export default App;
